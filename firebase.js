870f2f6026dd33c727de26faef5fb3b8b4749eeb
aman879 <64148207+aman879@users.noreply.github.com>
2024-07-09 14:59:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require("./firebase-key.json");
+const serviceAccount = require("/etc/secret/firebase-key.json");
 
 initializeApp({
     credential: cert(serviceAccount)
