8277892ff5e7c7f713419368e66d48e2158c573f
Aman kumar <aman9693kumar@gmail.com>
2024-07-09 14:33:37 +0530

    Final push

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const db = require('./firebase');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.post('/submit', async(req, res) => {
+    const {name, email, message}  = req.body;
+    console.log(name, email, message)
+    if (!name || !email || !message) {
+        return res.status(400).send("Name, email, or message missing");
+    }
+    try {
+        await db.collection('users').add({
+            name,
+            email,
+            message
+        });
+        res.status(200).send("Form submited succesfully");
+    } catch(error) {
+        console.log(error)
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
