4b437e3109ad25ee7786ec55f76ba96b41b65831
Aman kumar <aman9693kumar@gmail.com>
2024-07-09 15:27:29 +0530

    build

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require("/etc/secret/firebase-key.json");
+const serviceAccount = require("/etc/secrets/firebase-key.json");
 
 initializeApp({
     credential: cert(serviceAccount)
